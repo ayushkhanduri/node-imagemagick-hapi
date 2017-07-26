58fb555487a30ef00747559ba52f2897c480234c
ayushkhanduri khanduri <ayushkhanduri@gmail.com>
2017-07-26 14:17:32 +0530

    Unminified

--- webpack.config.js
+++ webpack.config.js
@@ -60,23 +60,23 @@ const webpackPackConfiguration = {
             }//
         ]
 
-    },
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            mangle: true,
-            compress: {
-                warnings: false, // Suppress uglification warnings
-                pure_getters: true,
-                unsafe: true,
-                unsafe_comps: true,
-                screw_ie8: true
-            },
-            output: {
-                comments: false,
-            },
-            exclude: [/\.min\.js$/gi] // skip pre-minified libs
-        })
-    ]
+    }
+    // plugins: [
+    //     new webpack.optimize.UglifyJsPlugin({
+    //         mangle: true,
+    //         compress: {
+    //             warnings: false, // Suppress uglification warnings
+    //             pure_getters: true,
+    //             unsafe: true,
+    //             unsafe_comps: true,
+    //             screw_ie8: true
+    //         },
+    //         output: {
+    //             comments: false,
+    //         },
+    //         exclude: [/\.min\.js$/gi] // skip pre-minified libs
+    //     })
+    // ]
 }
 
 module.exports = webpackPackConfiguration;
